fcdddd63cde69d602ee016a71c25c267b3599c1f
Piya Puengprapa <piya.puen@kkpfg.com>
2025-08-29 17:06:20 +0700

    feat: read file

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"Babe-Piya/tamboo/cipher"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	file, err := os.Open("data/fng.1000.csv.rot128")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	buffer := make([]byte, 1024)
+	fileDec, err := cipher.NewRot128Reader(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for {
+		n, errRead := fileDec.Read(buffer)
+		if errRead != nil {
+			if errRead == io.EOF {
+				break
+			}
+			log.Fatalf("Error reading chunk: %v", err)
+		}
+		fmt.Printf("Read %d bytes: %s\n", n, string(buffer[:n]))
+	}
+
 }
